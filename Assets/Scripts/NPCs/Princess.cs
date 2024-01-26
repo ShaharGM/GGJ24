ef55f707cc0644196e9f690863884db77ab6a70e
Or Magen <orm@ottopia.tech>
2024-01-26 19:42:18 +0200

    princess win\lose states

--- Assets/Scripts/NPCs/Princess.cs
+++ Assets/Scripts/NPCs/Princess.cs
@@ -1,40 +1,97 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
+using System.Collections;
 
 [System.Serializable]
+public class State
+{
+    public string dialogue;
+    public string animation;
+}
+
 public class Princess : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText; // Reference to the TextMeshPro Text object
     public Animator animator; // Reference to the Animator component
 
-    public int converstaionStep = -1;
-    public string[] dialog;
-    public string[] animationState;
+    public State[] start;
+    public State[] win;
+    public State[] lose;
 
-     void OnEnable()
+    public float swordSizeThreshold = 1;
+    public float sleepLength = 2f; // Sleep length in seconds
+
+    private float swordVal = 0f;
+    private bool isStartSceneRunning = false; // Flag to control the execution of the second Coroutine
+
+    void OnEnable()
     {
-        EventManager.onTalk.AddListener(Talk); // Subscribe to the onPlayerHit event
+        EventManager.onTalk.AddListener(Talk); // Subscribe to the onTalk event
     }
-    
+
     void OnDisable()
     {
-        EventManager.onTalk.RemoveListener(Talk); // Unsubscribe to the onPlayerHit event
+        EventManager.onTalk.RemoveListener(Talk); // Unsubscribe from the onTalk event
     }
-
 
     // This method is called when the "onTalk" event is invoked.
     public void Talk()
     {
-       converstaionStep = (converstaionStep + 1) % dialog.Length;
-       SetDialogueAndAnimation(dialog[converstaionStep], animationState[converstaionStep]);
+        swordVal = GetSwordSize();
+        StartCoroutine(RunStartScene());
     }
 
-    public void SetDialogueAndAnimation(string dialogue, string animationState)
+    public IEnumerator RunStartScene()
     {
-        // Change the text of the TextMeshPro Text object
-        dialogueText.text = dialogue;
+        // Set the flag to true to indicate that the start scene is running
+        isStartSceneRunning = true;
 
-        // Transition to a different animation state in the animator
-        animator.Play(animationState);
+        foreach (var state in start)
+        {
+            SetState(state);
+            yield return new WaitForSeconds(sleepLength);
+        }
+
+        // Start scene has ended, set the flag to false
+        isStartSceneRunning = false;
+
+        State[] endState = swordVal > swordSizeThreshold ? win : lose;
+
+        // Start the second Coroutine only if the start scene has ended
+        if (!isStartSceneRunning)
+        {
+            StartCoroutine(RunScene(endState));
+        }
+    }
+
+    public void SetState(State state)
+    {
+        dialogueText.text = state.dialogue;
+        animator.Play(state.animation);
+    }
+
+    public IEnumerator RunScene(State[] states)
+    {
+        for (int i = 0; i < states.Length; ++i)
+        {
+            SetState(states[i]);
+            yield return new WaitForSeconds(sleepLength); // Sleep for the specified duration
+        }
+    }
+
+    // Helper method to get the sword size from a GameObject with the "Sword" tag
+    private float GetSwordSize()
+    {
+        GameObject sword = GameObject.FindGameObjectWithTag("Sword");
+        if (sword != null)
+        {
+            return sword.transform.localScale.y;
+        }
+        else
+        {
+            Debug.LogWarning("Sword object not found");
+            return 0f; // Return a default value if the sword is not found
+        }
     }
 }
